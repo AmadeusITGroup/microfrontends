b3f36a8d127e45f81673714f99261bc16905e9df
Max Okorokov <max@okorokov.com>
2025-02-27 12:20:32 +0100

    refactor: simplify jest config

--- packages/angular/jest.config.ts
+++ packages/angular/jest.config.ts
@@ -1,35 +1,16 @@
-import { Config } from 'jest';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-globalThis.ngJest = {
-	skipNgcc: true,
-};
+import type { JestConfigWithTsJest } from 'ts-jest';
+import ngPreset from 'jest-preset-angular/presets';
 
 export default {
 	displayName: 'microfrontends-angular',
-	preset: 'jest-preset-angular',
+	...ngPreset.createCjsPreset({
+		tsconfig: '<rootDir>/lib/tsconfig.spec.json',
+	}),
 	roots: ['<rootDir>/lib/src'],
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-
-	// Angular setup
-	transform: {
-		'^.+\\.tsx?$': [
-			'jest-preset-angular',
-			{
-				tsconfig: '<rootDir>/lib/tsconfig.spec.json',
-				stringifyContentPathRegex: '\\.html$',
-			},
-		],
-	},
-	snapshotSerializers: [
-		'jest-preset-angular/build/serializers/no-ng-attributes',
-		'jest-preset-angular/build/serializers/ng-snapshot',
-		'jest-preset-angular/build/serializers/html-comment',
-	],
 
 	// Coverage
 	collectCoverageFrom: ['lib/src/**/*.ts'],
 	coverageDirectory: 'coverage',
 	coverageReporters: ['text', 'lcov'],
-} as Config;
+} as JestConfigWithTsJest;
